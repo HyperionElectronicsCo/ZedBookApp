9b33b3e525feacedb38e075d83fb91d76d0c33d7
HyperionElectronicsCo <HyperionElectronicsCo@hotmail.com>
2026-06-15 19:19:10 +0100

    ,

--- app/build/gen/com/hyperion/zedbook/BuildConfig.java
+++ app/build/gen/com/hyperion/zedbook/BuildConfig.java
@@ -3,8 +3,4 @@ package com.hyperion.zedbook;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
-    public final static String APPLICATION_ID = "com.hyperion.zedbook";
-    public final static String BUILD_TYPE = "debug";
-    public final static long VERSION_CODE = 2;
-    public final static String VERSION_NAME = "1.1-install-fix";
 }
